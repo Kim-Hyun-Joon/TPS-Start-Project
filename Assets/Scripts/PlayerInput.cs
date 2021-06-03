322a31f188eeeb7a097d3571962484af57b83445
김 현준 <jkajks123456@einssnc.com>
2021-06-03 18:10:17 +0900

    first commit

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public string fireButtonName = "Fire1";
+    public string jumpButtonName = "Jump";
+    public string moveHorizontalAxisName = "Horizontal";
+    public string moveVerticalAxisName = "Vertical";
+    public string reloadButtonName = "Reload";
+
+    public Vector2 moveInput { get; private set; }
+    public bool fire { get; private set; }
+    public bool reload { get; private set; }
+    public bool jump { get; private set; }
+    
+    private void Update()
+    {
+        if (GameManager.Instance != null
+            && GameManager.Instance.isGameover)
+        {
+            moveInput = Vector2.zero;
+            fire = false;
+            reload = false;
+            jump = false;
+            return;
+        }
+
+        //사이퍼즈에서 대각선으로 이동하는 스킬들이 더 빠른 이유
+        //대각선 이동하는 속도를 normalize하는 과정을 해주지 않아서
+        moveInput = new Vector2(Input.GetAxis(moveHorizontalAxisName), Input.GetAxis(moveVerticalAxisName));
+        if (moveInput.sqrMagnitude > 1) moveInput = moveInput.normalized;
+
+        jump = Input.GetButtonDown(jumpButtonName);
+        fire = Input.GetButton(fireButtonName);
+        reload = Input.GetButtonDown(reloadButtonName);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private CharacterController characterController;
+    private PlayerInput playerInput;
+    private Animator animator;
+
+    private Camera followCam;
+
+    public float speed = 6f;            //움직임 속도
+    public float jumpVelocity = 20f;    //점프시 속도
+    [Range(0.01f, 1f)] public float airControlPercent;
+
+    public float speedSmoothTime = 0.1f;
+    public float turnSmoothTime = 0.1f;
+
+    private float speedSmoothVelocity;
+    private float turnSmoothVelocity;
+
+    private float currentVelocityY;
+
+    public float currentSpeed =>
+        new Vector2(characterController.velocity.x, characterController.velocity.z).magnitude;
+
+    private void Start() {
+        playerInput = GetComponent<PlayerInput>();
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        followCam = Camera.main;
+    }
+
+    private void FixedUpdate() {
+        if (currentSpeed > 0.2f || playerInput.fire) Rotate();
+
+        Move(playerInput.moveInput);
+
+        if (playerInput.jump) Jump();
+    }
+
+    private void Update() {
+        UpdateAnimation(playerInput.moveInput);
+    }
+
+    public void Move(Vector2 moveInput) {
+        var targetSpeed = speed + moveInput.magnitude;
+        var moveDirection = Vector3.Normalize(transform.forward * moveInput.y + transform.right * moveInput.x);
+
+        var smoothTime = characterController.isGrounded ? speedSmoothTime : speedSmoothTime / airControlPercent;
+
+
+        currentVelocityY += Time.deltaTime * Physics.gravity.y;
+
+        var velocity = moveDirection * targetSpeed + Vector3.up * currentVelocityY;
+
+        characterController.Move(velocity * Time.deltaTime);
+
+        if (characterController.isGrounded) currentVelocityY = 0f;      //초기화 안하면 무한정 커진다
+
+    }
+
+    public void Rotate() {
+
+    }
+
+    public void Jump() {
+
+    }
+
+    private void UpdateAnimation(Vector2 moveInput) {
+
+    }
+}
